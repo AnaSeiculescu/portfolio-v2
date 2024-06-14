63183a61e7c244176917c2933b2611d932a65a99
Ana Seiculescu <anaseiculescu@gmail.com>
2024-06-14 13:15:52 +0300

    work on fixing the two carousels

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,30 +8,34 @@ export default function Work() {
         <Box className="work-container" sx={{ padding: "2% 2% 2% 35%" }}>
             <Box className="first-scroll-container">
                 <Box className="first-carousell-primary">
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "red" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "green" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "blue" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "red", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "green", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "blue", boxShadow: "4" }}></Card>
                 </Box>
                 <Box className="first-carousell-primary first-carousell-secondary">
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "red" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "green" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "blue" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "red", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "green", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "blue", boxShadow: "4" }}></Card>
                 </Box>
             </Box>
             <Box className="second-scroll-container">
                 <Box className="second-carousell-primary">
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "orange" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightgreen" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "purple" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightblue" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow", boxShadow: "4" }}></Card>
+                    <Card
+                        sx={{ width: "150px", height: "150px", backgroundColor: "lightgreen", boxShadow: "4" }}
+                    ></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "purple", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightblue", boxShadow: "4" }}></Card>
                 </Box>
                 <Box className="second-carousell-primary second-carousell-secondary">
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "orange" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightgreen" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "purple" }}></Card>
-                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightblue" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "yellow", boxShadow: "4" }}></Card>
+                    <Card
+                        sx={{ width: "150px", height: "150px", backgroundColor: "lightgreen", boxShadow: "4" }}
+                    ></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "purple", boxShadow: "4" }}></Card>
+                    <Card sx={{ width: "150px", height: "150px", backgroundColor: "lightblue", boxShadow: "4" }}></Card>
                 </Box>
             </Box>
         </Box>
