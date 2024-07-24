ab44ef538444c2d67a172bf2f834c215c2fb13f8
Ana Seiculescu <anaseiculescu@gmail.com>
2024-07-24 13:54:31 +0300

    for a viewport width smaller than 1300px the menu stays fixed on top

--- src/components/StickyMenu.jsx
+++ src/components/StickyMenu.jsx
@@ -6,31 +6,44 @@ import DownloadCV from "./DownloadCV";
 import GroupContactButtons from "./GroupContactButtons";
 import MadeWithLove from "./MadeWithLove";
 import "../StickyMenu.css";
+import { ViewportWidth } from "./ViewportWidth";
+import { useState, useEffect } from "react";
 
 export default function StickyMenu({ onTabChange, contentHeight, tabValue, setTabValue, onClickEmail }) {
+    const viewportWidth = ViewportWidth();
+    const [height, setHeight] = useState(contentHeight);
+
+    useEffect(() => {
+        if (viewportWidth > 1300) {
+            setHeight(contentHeight);
+        } else {
+            setHeight(110);
+        }
+    }, [viewportWidth, contentHeight]);
+
     const menuStyle = {
-        position: "absolute",
-        top: "0",
-        left: "10%",
+        // position: "absolute",
+        // top: "0",
+        // left: "10%",
 
-        display: "flex",
-        flexDirection: "column",
-        gap: "40px",
+        // display: "flex",
+        // flexDirection: "column",
+        // gap: "40px",
 
-        background:
-            "linear-gradient(0deg, rgba(172,55,255,1) 0%, rgba(14,6,37,1) 45%, rgba(71,96,217,1) 72%, rgba(34,193,195,1) 100%)",
-        color: "white",
-        boxShadow: 8,
-        borderRadius: "0",
+        // background:
+        //     "linear-gradient(0deg, rgba(172,55,255,1) 0%, rgba(14,6,37,1) 45%, rgba(71,96,217,1) 72%, rgba(34,193,195,1) 100%)",
+        // color: "white",
+        // boxShadow: 8,
+        // borderRadius: "0",
 
-        width: "18%",
-        height: contentHeight,
-        transition: "height 3.7s",
+        // width: "18%",
+        height: `${height}px`,
+        // transition: "height 3.7s",
 
-        textAlign: "center",
-        zIndex: "5",
-        opacity: "0.95",
-        fontFamily: "Nunito, sans-serif",
+        // textAlign: "center",
+        // zIndex: "5",
+        // opacity: "0.95",
+        // fontFamily: "Nunito, sans-serif",
     };
 
     return (
